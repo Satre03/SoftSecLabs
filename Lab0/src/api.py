5785e0ab042f5f6eefadd6b88f32d1b325a20b6b
Satre03 <Alexander@ztar.se>
2026-09-03 11:30:06 +0200

    change

--- Lab0/src/api.py
+++ Lab0/src/api.py
@@ -1,12 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-
-@app.route("/hello", methods=["GET"])
+@app.route("/hello")
 def hello():
-    return jsonify({"message": "Hello"})
-
+    name = request.args.get("name", "World")
+    return jsonify({"message": f"Hello {name}"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
--- Lab0/tests/test_hello.py
+++ Lab0/tests/test_hello.py
@@ -2,8 +2,7 @@ from src.api import app
 
 def test_hello():
     client = app.test_client()
-
-    response = client.get("/hello")
+    response = client.get("/hello?name=Anna")
 
     assert response.status_code == 200
-    assert response.get_json() == {"message": "Hello"}
+    assert response.get_json() == {"message": "Hello Anna"}
